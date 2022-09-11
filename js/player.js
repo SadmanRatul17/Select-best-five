ebf48c6d7e839b17340f594fc14fe53a857df23b
SadmanRatul17 <sadmanratul17@gmail.com>
2022-09-12 00:22:12 +0600

    add playerjs

---
+++ js/player.js
@@ -0,0 +1,73 @@
+// messi-- start
+document.getElementById('select-messi').addEventListener('click', function(){
+    const playerName = document.getElementById('messiName');
+    const name = playerName.innerText;
+
+    const location1 = document.getElementById('li-1');
+      location1.innerText=name;
+    
+
+})
+
+// messi --end
+
+
+document.getElementById('select-neymer').addEventListener('click', function(){
+    const playerName = document.getElementById('Neymer-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-2');
+     location.innerText= name;
+})
+
+
+document.getElementById('select-mbappe').addEventListener('click', function(){
+    const playerName = document.getElementById('Mbappe-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-3');
+     location.innerText= name;
+})
+document.getElementById('select-vitor').addEventListener('click', function(){
+    const playerName = document.getElementById('vitor-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-4');
+     location.innerText= name;
+})
+// 5
+document.getElementById('select-Ramos').addEventListener('click', function(){
+    const playerName = document.getElementById('Ramos-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-5');
+     location.innerText= name;
+})
+document.getElementById('select-renato').addEventListener('click', function(){
+    const playerName = document.getElementById('renato-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-5');
+     location.innerText= name;
+})
+document.getElementById('select-neymea').addEventListener('click', function(){
+    const playerName = document.getElementById('Neymera-name');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-2');
+     location.innerText= name;
+})
+document.getElementById('select-mbappea').addEventListener('click', function(){
+    const playerName = document.getElementById('Mbappe-namea');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-3');
+     location.innerText= name;
+})
+document.getElementById('select-vitora').addEventListener('click', function(){
+    const playerName = document.getElementById('vitor-namea');
+    const name = playerName.innerText;
+
+    const location = document.getElementById('li-4');
+     location.innerText= name;
+})
